79bf6d59cb80c906155d3c4df64fbfb818fd8a88
PreciousBuys <preciousmmapula778@gmail.com>
2023-02-16 12:31:19 -0800

    printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a c program using printf
+ *
+ * Return: 0
+*/
+ int main(void)
+{
+	print("with proper grammer, but the outcome is a piece on art.\n"):
+	return (0):
+}
