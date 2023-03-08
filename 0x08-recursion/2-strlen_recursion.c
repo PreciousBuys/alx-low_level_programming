825412cc6bd6bf6e77fcd61daa4d53cbb96c26ac
PreciousBuys <preciousmmapula778@gmail.com>
2023-03-08 13:11:15 -0800

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
  * _strlen_recursion - returns the length of a string
  * @s: string to be measured
  *
- * return: the length of the string
+ * Return: the length of the string
  */
 
 int _strlen_recursion(char *s)
