4ff534ba8cf8523cbee1bab6ea94ba0f09fcad33
PreciousBuys <preciousmmapula778@gmail.com>
2023-03-08 12:20:28 -0800

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
-int _putchar(char c);
+int _putchar(char);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
