4443f1abad3eb0bef37ec673a1c0e51df899458b
PreciousBuys <preciousmmapula778@gmail.com>
2023-04-09 06:17:41 -0700

    read text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,47 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - Read text file and print to STDOUT.
+ * @filename: The name of the text file being read.
+ * @letters: The number of letters to be read.
+ *
+ * Return: The actual number of bytes read and printed to STDOUT.
+ *         Returns 0 when the function fails or filename is NULL.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buf[letters];
+char *buf;
+ssize_t fd, w, t;
 
-	ssize_tnread, nwritten;
-
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-	{
-	return (0);
-	}
-
-	nread = read(fd, buf, letters);
-
-	if (nread == -1)
-	{
-	close(fd);
-	return (0);
-	}
-	nwritten = write(STDOUT_FILENO, buf, nread);
-	close(fd);
-	return ((nwritten == nread) ? nwritten : 0);
-	}
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+perror("Error: Can't open file");
+exit(1);
+}
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+{
+perror("Error: Can't allocate memory for buffer");
+exit(1);
+}
+t = read(fd, buf, letters);
+if (t == -1)
+{
+perror("Error: Can't read from file");
+free(buf);
+close(fd);
+exit(1);
+}
+w = write(STDOUT_FILENO, buf, t);
+if (w == -1) {
+perror("Error: Can't write to STDOUT");
+free(buf);
+close(fd);
+exit(1);
+}
+free(buf);
+close(fd);
+return (w);
+}
